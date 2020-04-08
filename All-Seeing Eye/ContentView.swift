dfce5e52afc87721ba9a0f33d55ca64786f888d9
Vlad Movileanu <vladislav.movileanu@ati.utm.md>
2020-04-08 16:15:56 +0300

    Initial Commit

---
+++ All-Seeing Eye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  All-Seeing Eye
+//
+//  Created by vladikkk on 08/04/2020.
+//  Copyright © 2020 TMPS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
